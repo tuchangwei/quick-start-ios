9353033fcef8d48e12907c2fa6e98e5ffdc4de53
Abir Majumdar <abir@layer.com>
2014-12-15 13:51:04 -0800

    Readded AppDelegates that accidentally removed

---
+++ QuickStart/LQSAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  QuickStart
+//
+//  Created by Abir Majumdar on 12/3/14.
+//  Copyright (c) 2014 Layer, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <LayerKit/LayerKit.h>
+#import "Constants.h"
+#import "LQSViewController.h"
+
+@interface LQSAppDelegate : UIResponder <UIApplicationDelegate,LYRClientDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
